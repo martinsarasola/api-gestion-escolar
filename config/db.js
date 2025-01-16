7bbffa57306eb2e47e44ed118da9d4addcc355b4
Martín Sarasola <martin.sarasola01@hotmail.com>
2025-01-16 18:45:46 -0300

    Modified files for Vercel Deployment

--- config/db.js
+++ config/db.js
@@ -5,10 +5,16 @@ const MONGO_URI = process.env.MONGO_URI;
 
 const dbconnect = async () => {
   try {
-    await mongoose.connect(MONGO_URI);
-    console.log("Conexión exitosa a la base de datos.");
+    await mongoose.connect(MONGO_URI, {
+      connectTimeoutMS: 5000,
+      socketTimeoutMS: 5000,
+      serverSelectionTimeoutMS: 5000,
+      maxPoolSize: 10,
+    });
+    console.log("Conexión con la base de datos exitosa");
   } catch (error) {
-    console.error("Hubo un error al conectarse a la base de datos: " + error);
+    console.error("Hubo un error al conectar a la base de datos:", error);
+    throw error;
   }
 };
 
